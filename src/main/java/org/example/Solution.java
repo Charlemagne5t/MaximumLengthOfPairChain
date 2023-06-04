6fb020d4fed23182e8eaeedb110c9a1aa90a12d1
alek <wait82985@gmail.com>
2023-06-04 15:50:37 +0600

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
     public int findLongestChain(int[][] pairs) {
-        //TODO
-        return 0;
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[0]));
+
+        int[] dp = new int[pairs.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < dp.length; i++) {
+            int maxSubSeq = 1;
+            for (int j = 0; j < i; j++) {
+
+                if (pairs[j][1] < pairs[i][0]) {
+                    maxSubSeq = Math.max(maxSubSeq, dp[j] + 1);
+                }
+            }
+            dp[i] = maxSubSeq;
+        }
+        System.out.println(Arrays.toString(dp));
+
+        return dp[dp.length - 1];
     }
 }
